35591b4a9012e427a293924a1f1b3d2afa22287a
Fajar Riza Fauzi <fajarizaf@gmail.com>
2024-10-20 13:34:33 +0700

    add log module

--- controllers/email-log.js
+++ controllers/email-log.js
@@ -3,30 +3,31 @@ const models = require('../models')
 const Emaillog = models.emaillog
 
 
-exports.createLogs = (to, subject, req, res) => new Promise((resolve, reject) => {
-    
+exports.createLogs = (to, subject, module_id, req, res) => new Promise((resolve, reject) => {
+
     Emaillog.create({
         to: to,
         subject: subject,
+        module_id: module_id,
         req: JSON.stringify(req.body),
         res: JSON.stringify(res),
     })
-    .then((respond) => {
-        resolve(
-            convertToJson({
-                respond: {
-                    status: 'success'
-                }
-            })
-        )
-    })
-    .catch((e) => {
-        resolve(
-            convertToJson(
-                {respond: {status: 'error', response: e.message}}
+        .then((respond) => {
+            resolve(
+                convertToJson({
+                    respond: {
+                        status: 'success'
+                    }
+                })
             )
-        )
-    })
+        })
+        .catch((e) => {
+            resolve(
+                convertToJson(
+                    { respond: { status: 'error', response: e.message } }
+                )
+            )
+        })
 
 })
 
